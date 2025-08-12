58b8e213199c5e3ef2757dfed02691fbe9c9fbb5
Adrienne Pagaran <pagaranag@students.nu-laguna.edu.ph>
2025-08-12 22:07:21 +0800

    basic and doodles
    simple print,scanner and for loop

---
+++ basics day1.py
@@ -0,0 +1,26 @@
+print("Hello, World!")
+# This is a simple Python script that prints "Hello, World!" to the console.
+# num_elements = int(input("Enter the number of elements: "))
+# elements = []
+
+# for _ in range(num_elements):
+#     element = int(input("Enter an element: "))
+#     elements.append(element)
+
+# sorted_elements = sorted(elements)
+# print("Sorted array:", sorted_elements)
+
+
+#basic scanner 
+
+# name_element = (input("Enter the name of the element: "))
+# age_element = int(input("Enter the age of the element: "))
+# course_element = input("Enter the course of the element: ")
+# print("Name:", name_element)
+# print("Age:", age_element)
+# print("Course:", course_element)
+
+# Simple for loop example
+for i in range(5):
+    print(f"Iteration {i + 1}: Hello from the loop!")
+
